f74bb49ea23b3cf48363ff2bd7aa5f25c3532f7c
orendzi <olga.golubovskaya@ironsoft.su>
2014-09-07 11:22:32 +0000

    fibonacci

--- js/fib.js
+++ js/fib.js
@@ -1,24 +1,51 @@
 // Fibonacci
 
-var newElement = document.getElementsByTagName('body');
-console.log(newElement);
 // for-cycle
 function fib(a, b, n) {
     var fibArray = [];
-    for(var i=0; i<n; i++) {
+    for(var i=0; i<n+1; i++) {
         var temp = a;
         a = b;
         b = temp + b;
-        fibArray.push(a);
+        fibArray.push(temp);
     }
    return fibArray; 
 }
 
-function results (e, array) {
-    e.innerHTML = "<p>" + array + "</p>";
+function results (array) {
+    var parentE = document.body;
+    var container = document.createElement('p');
+    container.innerHTML = array;
+    parentE.appendChild(container);
 }
 
 
 // 1recursion
+function fibRecurse(array, a, b, n) {
+    array.push(a);
+    if (array.length<n) {
+        fibRecurse(array, b, a+b, n);
+    }
+    return array;
+}
+
 
 // 2recursion
+function fibNewRecurse(array, a, b, max) {
+    array.push(a);
+    if (b < max) {
+        fibNewRecurse(array, b, a+b, max);
+    }
+    return array;
+}
+
+// Init
+
+function init () {
+    results(fib(0, 1, 8));
+    results(fibRecurse([], 0, 1, 16));
+    results(fibNewRecurse([], 0, 1, 234));
+}
+
+document.addEventListener( "DOMContentLoaded", init, false );
+
